f429c58220b3ac2bf55af3ae6d4a7db91b2dc1a1
ffergomez <fermingomezmoyano55@gmail.com>
2024-09-20 18:35:09 -0300

    Commit inicial del proyecto Web Api

---
+++ Data/Repository/ILibroRepository.cs
@@ -0,0 +1,15 @@
+﻿using EFWebApi.Data.Models;
+
+namespace EFWebApi.Data.Repository
+{
+    public interface ILibroRepository
+    {
+        void Create(Libro libro);
+        void Update(Libro libro);
+        List<Libro> GetAll();
+        Libro? GetById(int id);
+
+        void Delete(int id);
+
+    }
+}
---
+++ Data/Repository/LibroRepository.cs
@@ -0,0 +1,50 @@
+﻿using EFWebApi.Data.Models;
+
+namespace EFWebApi.Data.Repository
+{
+    public class LibroRepository : ILibroRepository
+    {
+        private LibrosDbContext _context;
+
+        public LibroRepository(LibrosDbContext context)
+        {
+            _context = context;
+        }
+        public void Create(Libro libro)
+        {
+            _context.Libros.Add(libro);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var libroDelete = GetById(id);
+            if(libroDelete != null)
+            {
+                _context.Libros.Remove(libroDelete);
+                _context.SaveChanges();
+            }
+            
+        }
+
+        public List<Libro> GetAll()
+        {
+            return _context.Libros.ToList();
+        }
+
+        public Libro? GetById(int id)
+        {
+            return _context.Libros.Find(id);
+        }
+
+        public void Update(Libro libro)
+        {
+            
+            if (libro != null)
+            {
+                _context.Libros.Update(libro);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
